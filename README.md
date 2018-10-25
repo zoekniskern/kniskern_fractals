7b002496458b0e8a1af8df11cde6fe9dcaf6b967
zoekniskern <zxk3766@rit.edu>
2018-10-25 01:25:10 -0400

    project with README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # kniskern_fractals
 Experimenting with fractals
+
+///////////////////////
+Aesthetic Goals:
+For this project I wanted to experiment with a relationship between symmetry and fractals. Drawing on inspiration from symmetric drawing programs and the Rorschach test, I created a visual that incorporates variations of koch curves within a symmetric framework. The visual invites the audience to see patterns and emerging imagery in the overlap of opacity and vibrance through the generations of iterations.
+
+///////////////////////
+Development Process:
+I knew that I wanted there to be a clear user interaction so building koch curves that would appear based on click events was the first step. This is broken down into the actual koch curve, making a related group of koch curves, and placing the group related to the user click.
+
+I felt like a circle would be a clean and achievable base shape so I created an arc that then clips the koch curves that are drawn. Using getImage, this arc and it's visual content becomes maniputable pixels which enabled me to mirror the image.
+
+To create variety I included random variables that influence the size, rotation and visual properties of the curves.
+
+///////////////////////
+Reflections:
+The end result is interesting and incorporates principles that we learned. Because I ended up changing my initial idea so much it detracted from the amount of time I spent actually developing the concept. I am satisfied with the visual output but the code itself is very heavy handed and expensive to run. Definite room for improvement.
+
+
+
--- app.js
+++ app.js
@@ -3,7 +3,37 @@
 const canvas = document.getElementById("mainCanvas");
 const ctx = mainCanvas.getContext('2d', {alpha: false});
 
+const bcanvas = document.getElementById("backupCanvas");
+const bctx = bcanvas.getContext('2d', {alpha: false});
+
 let w, h, maxIterations; 
+let midX, midY;
+
+let curMouse = {};
+
+let counter = 0;
+let iterations = 1;
+
+let segLen = 1 / 3;
+
+var radius=300;
+
+let kochPoints;
+
+let varAng;
+
+let run = true;
+
+let Strokecolors = ['pink', 'lightpink', 'palevioletred'];
+
+window.addEventListener('resize', updateCanvasSize());
+document.addEventListener("click", getMousePos);
+document.addEventListener("click", clickSquare);
+document.getElementById("clear").onclick = updateCanvasSize;
+
+updateCanvasSize();
+
+requestAnimationFrame(animate);
 
 /* UTILITY CODE */
 
@@ -13,24 +43,157 @@ function updateCanvasSize() {
   h = window.innerHeight;
   canvas.width = w;
   canvas.height = h;
+  bcanvas.width = w;
+  bcanvas.height = h;
+
+  midX = w/2;
+  midY = h/2;
 
   maxIterations = 1;
+  kochPoints = [];
 
-  while((w / Math.pow(3, maxIterations) > 1)) {
+  while((w / Math.pow(5, maxIterations) > 1)) {
     maxIterations += 1;
   }
+
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, w, h);
+
+  varAng = lerp(1,5);
+
 }
-updateCanvasSize();
-window.addEventListener('resize', updateCanvasSize());
+
+//get a random integer between two numbers
+function lerp(a,b) {
+  return Math.floor(Math.random() * b) + a;
+}
+
+function drawSquare(x,y,side) {
+
+  ctx.strokeStyle = 'red';
+  ctx.strokeWidth = 5;
+  
+  ctx.save();
+  ctx.beginPath()
+  ctx.moveTo(x - side/2, y - side/2); //TOP LEFT
+  ctx.lineTo(x + side/2, y - side/2); //TOP RIGHT
+  ctx.lineTo(x + side/2, y + side/2); //BOTTOM RIGHT
+  ctx.lineTo(x - side/2, y + side/2); //BOTTOM LEFT
+  ctx.closePath();
+  ctx.stroke();
+
+  //console.log('draw square ran');
+
+}
+
+function getMousePos(event) {
+
+  let mouse = {
+    x: event.clientX,
+    y: event.clientY
+  };
+  
+  curMouse = mouse;
+  //console.dir(mouse);
+  //return  mouse;
+}
+
+// function clear(){
+//   kochPoints = [];
+//   console.log('CLear ran');
+//   console.dir(kochPoints);
+// }
+
+function clickSquare() {
+  //getMousePos();
+  //drawSquare(curMouse.x, curMouse.y, 100);
+  //koch(curMouse.x, curMouse.y, 50)
+  let newP = {
+    x: curMouse.x,
+    y: curMouse.y
+  }
+
+  kochPoints.push(newP);
+
+  //console.dir(kochPoints);
+}
+
 
 /* MAIN DRAWING CODE */
+function draw(){
+  for(var i=0;i<1;i++){
+    ctx.beginPath();
+
+    ctx.moveTo(midX, midY);
+    //ctx.arc(midX, midY,radius,i*Math.PI/4,(i+1)*((Math.PI/4)),false);
+    ctx.arc(midX, midY,radius, 90, 270, false);  
+    ctx.closePath();
+    ctx.clip();
+
+    allKoch();
+    // ctx.fillStyle = 'gray';
+    // ctx.fillRect(0, 0, w, h);
+
+    ctx.save()
+    ctx.fillStyle = 'crimson';
+    ctx.globalAlpha = .1;
+    ctx.beginPath();
+
+    ctx.moveTo(midX, midY);
+    ctx.arc(midX, midY,radius, 180, 90, true);  
+    ctx.closePath();
+    ctx.fill();
+    ctx.restore();
+
+    //allKoch();
+
+  }
+}
+
+function allKoch(){
 
 
+  ctx.strokeStyle = 'lightpink';
+  //ctx.lineWidth = 2;
 
-function drawKochCurve1(width, iterations) {
+  // add some blurring  
+  ctx.shadowBlur = 10;
+  ctx.shadowColor = 'darkmagenta';
 
-  // multiplier for seg lens.  
-  let segLen = 1 / 3;
+  let sideLength;
+
+  for(let j = 0; j < kochPoints.length; j ++){
+    ctx.lineWidth = lerp(.5,5);
+    sideLength = lerp(10,150);
+    //console.log(sideLength);
+    cInd = lerp(0,Strokecolors.length);
+    ctx.strokeStyle = Strokecolors[cInd];
+    //console.log(ctx.strokeStyle);
+    koch(kochPoints[j].x, kochPoints[j].y, sideLength);
+  }
+}
+
+function koch(cX, cY, side){
+  
+  kochLine(cX - side,cY - side, side, 0 * Math.PI/varAng);
+  kochLine(cX + side,cY - side, side, 2 * Math.PI/varAng);
+  kochLine(cX + side,cY + side, side, 4 * Math.PI/varAng);
+  kochLine(cX - side,cY + side, side, 6 * Math.PI/varAng);
+
+};
+
+function kochLine( xForm, yForm, side,rot){
+  ctx.save();
+  ctx.translate(xForm, yForm);
+  ctx.rotate(rot);
+  ctx.beginPath();
+  drawKochCurve(side*2, iterations);
+  ctx.closePath();
+  ctx.stroke();
+  ctx.restore();
+}
+
+function drawKochCurve(width, iterations) {
 
   // this is the exit condition for the recursion
   // when we hit bottom, we draw the line
@@ -49,131 +212,73 @@ function drawKochCurve1(width, iterations) {
 
     //TYPE 1
     // 1st segment
-    drawKochCurve1(newWidth, newIterations);
+    drawKochCurve(newWidth, newIterations);
 
     // 2nd segment
     ctx.translate(newWidth, 0);
-    ctx.rotate(-Math.PI * 1/2);  // 60 degrees counterclockwise
-    drawKochCurve1(newWidth, newIterations);
+    ctx.rotate(-Math.PI * 1/2); 
+    drawKochCurve(newWidth*2, newIterations);
 
     // 3rd segment
-    ctx.translate(newWidth, 0);
-    ctx.rotate(Math.PI * 1/2);  // 120 degrees clockwise
-    drawKochCurve1(newWidth, newIterations);
+    ctx.translate(newWidth*2, 0);
+    ctx.rotate(Math.PI * 1/2);
+    drawKochCurve(newWidth, newIterations);
 
     // 4th segment
     ctx.translate(newWidth, 0);
-    ctx.rotate(-Math.PI * -1/2);  // 60 degrees counterclockwise
-    drawKochCurve1(newWidth, newIterations);
+    ctx.rotate(-Math.PI * -1/2);
+    drawKochCurve(newWidth*2, newIterations);
 
     // 5th segment
     ctx.translate(newWidth, 0);
-    ctx.rotate(-Math.PI * 1/2);  // 60 degrees counterclockwise
-    drawKochCurve1(newWidth, newIterations);
+    ctx.rotate(-Math.PI * 1/2);
+    drawKochCurve(newWidth, newIterations);
 
     ctx.restore();
   }
-}
-
-
-let counter = 0;
-let iterations = 1;
-
-function draw() {
-    
-    ctx.fillStyle = '#000014';
-    ctx.fillRect(0,0,w,h);
-
-    ctx.strokeStyle = 'pink';
-    ctx.lineWidth = 2;
-
-    // add some blurring  
-    ctx.shadowBlur = 10;
-    ctx.shadowColor = '#00c';
-
-    //Type 1
-    ctx.save();
-
-    let segment = w/4;
-    //INNER SQUARE
-    //top
-    ctx.translate(w/2,0);
-    ctx.rotate(45 * Math.PI / 180)
-    ctx.translate(segment, segment);
-    ctx.beginPath();
-    drawKochCurve1(w/4, iterations);
-    ctx.stroke();
-
-    //right side
-    ctx.rotate(Math.PI/2)
-    ctx.translate(0, -segment);
-    ctx.beginPath();
-    drawKochCurve1(w/4, iterations);
-    ctx.stroke();
-
-    //bottom
-    ctx.rotate(Math.PI/2)
-    ctx.translate(0, -segment);
-    ctx.beginPath();
-    drawKochCurve1(w/4, iterations);
-    ctx.stroke();
-
-    //left side
-    ctx.rotate(Math.PI/2)
-    ctx.translate(0, -segment);
-    ctx.beginPath();
-    drawKochCurve1(w/4, iterations);
-    ctx.stroke();
-
-    ctx.restore();
-
-
-    ctx.save();
-    segment = w/2;
-    //OUTER SQUARE
-    //top
-    ctx.translate(w + w/2/2,-h/4.5);
-    ctx.rotate(Math.PI / 2)
-    ctx.translate(segment, segment);
-    ctx.beginPath();
-    drawKochCurve1(w/2, iterations);
-    ctx.stroke();
-
-    //right side
-    ctx.rotate(Math.PI/2)
-    ctx.translate(0, -segment);
-    ctx.beginPath();
-    drawKochCurve1(w/2, iterations);
-    ctx.stroke();
-
-    //bottom
-    ctx.rotate(Math.PI/2)
-    ctx.translate(0, -segment);
-    ctx.beginPath();
-    drawKochCurve1(w/2, iterations);
-    ctx.stroke();
-
-    //left side
-    ctx.rotate(Math.PI/2)
-    ctx.translate(0, -segment);
-    ctx.beginPath();
-    drawKochCurve1(w/2, iterations);
-    ctx.stroke();
-
-    ctx.restore();
-  
 
 }
 
-function animate(t) {
-  if((counter % 120) == 0) {
-    draw();
-    if(++iterations > maxIterations) {
-      iterations = 1;  
+function flipData(imgData){
+  //https://permadi.com/2009/04/html5-using-canvas-to-flip-images/
+  for (i=0; i<imgData.height; i++)
+  {
+   // We only need to do half of every row since we're flipping the halves
+    for (j=0; j<imgData.width/2; j++)
+    {
+       var index=(i*4)*imgData.width+(j*4);
+       var mirrorIndex=((i+1)*4)*imgData.width-((j+1)*4);
+       for (p=0; p<4; p++)
+       {
+         var temp=imgData.data[index+p];
+         imgData.data[index+p]=imgData.data[mirrorIndex+p];
+         imgData.data[mirrorIndex+p]=temp;
+       }
     }
   }
-  counter++;
-  requestAnimationFrame(animate)
 }
 
-requestAnimationFrame(animate);
+function animate() {
+  //draw();
+    if((counter % 200) == 0) {
+      //allKoch();
+      draw();
+      if(++iterations > maxIterations) {
+        iterations = 1;  
+      }
+    }
+    counter++;
+
+    ctx.save();
+    var imgData=ctx.getImageData(0,0,w,h);
+
+    flipData(imgData);
+
+    ctx.putImageData(imgData,0,0,0,0,imgData.width + 300, imgData.height + 300);
+    ctx.restore();
+
+    requestAnimationFrame(animate);
+}
+
+
+
